5b9252779a10057d43d1bc07238ce2b24d4c65b1
Kristina Hristovska <kristina.hristovska@outlook.com>
2019-03-23 19:18:03 +0100

    First part of the test to do list app

---
+++ app/src/main/java/com/example/pet_app/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.pet_app
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val thingsToDoList = arrayListOf("")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        addNewThingBtn.setOnClickListener {
+            val newThing = newThingTxt.text.toString()
+            thingsToDoList.add(newThing)
+            newThingTxt.text.clear()
+        }
+
+        viewToDoListBtn.setOnClickListener {
+            val intent= Intent(this, ToDoListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
